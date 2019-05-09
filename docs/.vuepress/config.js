a59f910e7f718469ba2be6ff03883bec46ac10e6
张新永 <xinyong.zhang1@baozun.com>
2019-05-09 20:00:29 +0800

    feat: 添加广告

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,11 @@
 module.exports = {
     title: 'Hello',
-    description: '百度一下你就知道了',
+    description: '云服务器',
+    head: [
+        ['script', { async: 'async', src: '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js' }],
+        ['script', {}, ` (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-8620617039148215",
+            enable_page_level_ads: true
+        });`]
+    ]
 }
